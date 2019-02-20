7c53d50390dc50e2e050b0d8c5b454f511a9a3f5
tmmgeek <2015rpro@gmail.com>
2019-02-20 15:46:01 -0500

    completed task 3-David

--- higherOrderFunctionDrill.js
+++ higherOrderFunctionDrill.js
@@ -13,3 +13,27 @@ function goodbye(){
 
 repeat(hello, 5);
 repeat(goodbye, 5);
+
+
+function filter(arr, fn){
+	let newArray = [];
+	for(let i = 0; i < arr.length;i++){
+		if(fn(arr[i])){
+			newArray.push(arr[i]);
+		}
+	}
+	return newArray;
+}
+
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+    // This is known as a "predicate function" - it's a function that 
+    // only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
